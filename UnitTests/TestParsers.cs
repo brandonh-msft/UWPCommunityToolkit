afb83de3d8cf017e78eecbadd0c6218781ba760d
Brandon H <hurlburb@microsoft.com>
2016-10-24 11:33:13 -0700

    Stylecop ignoring as did review call stack.

--- UnitTests/TestParsers.cs
+++ UnitTests/TestParsers.cs
@@ -6,6 +6,7 @@ namespace UnitTests
     internal class TestDeepLinkParser : DeepLinkParser
 #pragma warning restore SA1649 // File name must match first type name
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors", Justification = "Call stack reviewed.")]
         public TestDeepLinkParser(string uri)
         {
             ParseUriString(uri);
@@ -16,6 +17,7 @@ namespace UnitTests
     internal class TestCollectionCapableDeepLinkParser : CollectionFormingDeepLinkParser
 #pragma warning restore SA1402 // File may only contain a single class
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors", Justification = "Call stack reviewed.")]
         public TestCollectionCapableDeepLinkParser(string uri)
         {
             ParseUriString(uri);
